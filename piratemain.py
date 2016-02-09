b56378ac4c92fe9cdc84af360deed212740f34d8
shawnruss <shawnruss@gmail.com>
2016-02-09 12:52:39 +0000

    first function done

--- piratemain.py
+++ piratemain.py
@@ -1,9 +1,16 @@
-
+questions = {
+    "strong": "Do ye like yer drinks strong?",
+    "salty": "Do ye like it with a salty tang?",
+    "bitter": "Are ye a lubber who likes it bitter?",
+    "sweet": "Would ye like a bit of sweetness with yer poison?",
+    "fruity": "Are ye one for a fruity finish?",}
 
 def GetDrinkStyle(): 
    """determine the style of drink the customer likes """
-   
-   input("Enter something: ")
-   return 0
+   styleResponses = {}
+   for drinkstylequestions in questions:
+      styleResponses[drinkstylequestions] = input(questions[drinkstylequestions] ) in ("y", "yes")
+
+   return styleResponses
    
 print(GetDrinkStyle())
